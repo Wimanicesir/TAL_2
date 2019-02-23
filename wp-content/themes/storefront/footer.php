01f2d1abc276873d5c4b9b03b3acc155de0f1bae
Kaduk <iamkodec@gmail.com>
2019-02-23 12:08:22 +0100

    css changes

--- wp-content/themes/storefront/footer.php
+++ wp-content/themes/storefront/footer.php
@@ -17,112 +17,74 @@
 
 	<footer id="colophon" class="TAL-footer" role="contentinfo">
 		<div class="col-full row">
-        <div class="col-lg-3">
-        <h1>The Animation Library</h1>
-        <span> 	&copy; 2019 theanimationlibrary.com</span>
-        <div class="social d-none d-md-block">
-            <div class="social__facebook">
-                <a href="https://www.facebook.com/TheAnimationLibrary/" target="_blank"><i class="fa fa-facebook"></i></a>
+            <div class="col-md-3 text-center">
+                <h1 class="footer-logo"><a href="#">The Animation Library</a></h1>
+                <span>&copy; 2019 theanimationlibrary.com</span>
+                <div class="">
+                    <div class="social__facebook">
+                        <a href="https://www.facebook.com/TheAnimationLibrary/" target="_blank"><i class="fa fa-facebook"></i></a>
+                    </div>
+                    <div class="social__pinterest">
+                        <a href="https://www.pinterest.com/theanimationlibrary/?eq=The&etslf=6668" target="_blank"><i class="fa fa-pinterest"></i></a>
+                    </div>
+                    <div class="social__youtube">
+                        <a href="https://www.youtube.com/channel/UCtAT4SE9qxAf_Oxxg5P95Zw" target="_blank"><i class="fa fa-youtube"></i></a>
+                    </div>
+                </div>
             </div>
-            <div class="social__pinterest">
-                <a href="https://www.pinterest.com/theanimationlibrary/?eq=The&etslf=6668" target="_blank"><i class="fa fa-pinterest"></i></a>
+            <div class="col-md-3">
+                <div class="TAL-footer__sitemap">
+                    <h5>The Animation Library</h5>
+                    <ul>
+                        <li>Shop</li>
+                        <li>Credits</li>
+                        <li>Tips & Tricks</li>
+                    </ul>
+                </div>
             </div>
-            <div class="social__youtube">
-                <a href="https://www.youtube.com/channel/UCtAT4SE9qxAf_Oxxg5P95Zw" target="_blank"><i class="fa fa-youtube"></i></a>
+            <div class="col-md-3">
+                <div class="TAL-footer__legal">
+                    <h5>Legal</h5>
+                    <ul>
+                        <li>Privacy Policy</li>
+                        <li>Terms of use</li>
+                        <li>Cookie Policy</li>
+                    </ul>
+                </div>
             </div>
-        </div>
-        </div>
-        <div class="col-lg-3">
-            <div class="TAL-footer__sitemap">
-                <ul>
-                    <li><b>The Animation Library</b></li>
-                    <li>Shop</li>
-                    <li>Credits</li>
-                    <li>Tips & Tricks</li>
-                </ul>
-            </div>
-        </div>
-        <div class="col-lg-3">
-            <div class="TAL-footer__legal">
-            <ul>
-                <li>Legal</li>
-                <li>Privacy Policy</li>
-                <li>Terms of use</li>
-                <li>Cookie Policy</li>
-            </ul>
-            </div>
-        </div>
-        <div class="col-lg-3">
-        <h4>The best animation tips in your inbox</h4>
+            <div class="col-md-3">
+                <div class="TAL-footer__cta">
+                    <h5>The best animation tips in your inbox</h5>
                     <form action="https://theanimationlibrary.us17.list-manage.com/subscribe/post?u=8fd62a0668d6ec2f7d17d69e4&amp;id=8b125fcec9"
-                        method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate"
-                        target="_blank" novalidate>
-						<button type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="TALbutton btn-yellow hvr-grow-shadow"
-                            action="submit">
+                            method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate"
+                            target="_blank" novalidate>
+                        <button type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="TALbutton btn-yellow hvr-grow-shadow"
+                                action="submit">
                             <p>Subscribe</p>
                         </button>
 
                         <div id="mc_embed_signup_scroll">
-                            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email"
-                                required>
+                            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email" required>
                         </div>
                         <div id="mce-responses" class="clear">
                             <div class="response" id="mce-error-response" style="display:none"></div>
                             <div class="response" id="mce-success-response" style="display:none"></div>
                         </div>
 
-                        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_8fd62a0668d6ec2f7d17d69e4_8b125fcec9"
-                                tabindex="-1" value=""></div>
-
-                </div>
-                </form>
-
-        </div>
-
-        <!--
-		<div class="col-lg-6">
-			<ul class="TAL-footer__links">
-				<li><a href="https://www.theanimationlibrary.org/test/privacy-policy/">Privacy Policy</a></li>
-				<li><a href="https://www.theanimationlibrary.org/test/terms-of-use/">Terms of use</a></li>
-				<li><a href="https://www.theanimationlibrary.org/test/cookie-policy/">Cookie Policy</a></li>
-				<li><a href=" <?php //echo $path ?>/sitemap.html/">Sitemap</a></li>
-			</ul>
-		</div>
-		<div class="col-lg-6">
-
-                <div id="mc_embed_signup" class="TAL-footer__cta">
-				<h4>Subscribe to our newsletter</h4>
-                    <form action="https://theanimationlibrary.us17.list-manage.com/subscribe/post?u=8fd62a0668d6ec2f7d17d69e4&amp;id=8b125fcec9"
-                        method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate"
-                        target="_blank" novalidate>
-						<button type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="TALbutton btn-yellow hvr-grow-shadow"
-                            action="submit">
-                            <p>Subscribe</p>
-                        </button>
-
-                        <div id="mc_embed_signup_scroll">
-                            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email"
-                                required>
+                        <div style="position: absolute; left: -5000px;" aria-hidden="true">
+                            <input type="text" name="b_8fd62a0668d6ec2f7d17d69e4_8b125fcec9" tabindex="-1" value="">
                         </div>
-                        <div id="mce-responses" class="clear">
-                            <div class="response" id="mce-error-response" style="display:none"></div>
-                            <div class="response" id="mce-success-response" style="display:none"></div>
-                        </div>
-
-                        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_8fd62a0668d6ec2f7d17d69e4_8b125fcec9"
-                                tabindex="-1" value=""></div>
-
+                    </form>
                 </div>
-                </form>
             </div>
-        -->
-</div>
+        </div>
+    </footer>
 
 <?php wp_footer(); ?>
-        <script
-                src="https://code.jquery.com/jquery-3.3.1.min.js"
-                integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-                crossorigin="anonymous"></script>
+<script
+    src="https://code.jquery.com/jquery-3.3.1.min.js"
+    integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+    crossorigin="anonymous"></script>
 <script>
 
 $( document ).ready(function() {
