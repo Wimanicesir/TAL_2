811c200ded80820edd86412d1ae6335376bc7d63
Kaduk <iamkodec@gmail.com>
2019-03-02 17:19:22 +0100

    credit & orders styling

--- wp-content/themes/storefront/page-575.php
+++ wp-content/themes/storefront/page-575.php
@@ -13,18 +13,83 @@ $url = get_home_url();
 	</ul>
 	<span>Log out</span>
 </div>
+
 <div class="credits">
     <div class="credits__text">
         <h2>Credits</h2>
-        <p>You have <b><?php echo do_shortcode( '[user_credits]' );?></b> credits</p>
+        <p>You have <b><?php echo do_shortcode( '[user_credits]' );?></b> credits left</p>
     </div>
 
     <div class="credits__form">
     <div class="credits__form__title">
-        <h2>Buy credits</h2>    
+        <h2>Buy credits</h2>
+    </div>
+        <form class="buyCreditsForm" action="">
+        
+        </form>
+        <button class="buyCreditsButton">Buy <span class="amountCredits">6</span> credits</button>    
     </div>
     <?php echo do_shortcode( '[buy_credits]' );?>
     </div>
 </div>
 
+
+
+
 <?php get_footer(); ?>
+
+<script>
+$(document).ready(function(){
+    // Get all credits rows
+    var rows = $('.credit-row');
+
+    // Init globals
+    var labels = [];
+    var prices = [];
+    var amountCredits = [];
+    var amountPrices= [];
+    var pricePerCredits = [];
+    var buttons = [];
+
+    // Split up rows into globals
+    for (var i = 0; i < rows.length; i++){
+        labels.push($(rows[i]).find('.credits')[0].innerText);
+        prices.push($(rows[i]).find('.price')[0].innerText);
+        buttons.push($(rows[i]).find('.buy-now-btn')[0].firstChild);
+    }
+
+    // Create form
+    for (var j =0; j < labels.length; j++){
+        // Get integer values
+        amountCredits.push(labels[j].substr(0,labels[j].indexOf(' ')));
+        amountPrices.push(prices[j].substr(1,prices[j].indexOf(',') - 1));
+        pricePerCredits.push(parseFloat(amountPrices[j] / amountCredits[j]).toPrecision(3));
+
+        // Prepare HTML
+        var checkboxOption = '<input type="radio" name="credits" data-index="' + j + '" value="'+ amountCredits[j] +'">' + '<span class="credits__form__row__label">' +  labels[j] + '</span>';
+        var price = '<span class="credits__form__row__price">€'+ amountPrices[j] + '</span>';
+        var ppc = '<span class="credits__form__row__ppc">€'+ pricePerCredits[j] +'/credit</span>';
+
+        // Append new row to form
+        $('.buyCreditsForm').append('<div id="cr-'+ j +'" class="credits__form__row"></div>');
+        var newRow = $('#cr-' + j);
+        newRow.append(checkboxOption);
+        newRow.append(price);
+        newRow.append(ppc);
+        newRow.append('<br />');
+
+    }
+
+    // On change buy credits form
+    $('.buyCreditsForm').on('change', function(){
+        var selectedCredits = document.querySelector('input[name="credits"]:checked').value;
+        $('.amountCredits').html(selectedCredits);
+    });
+
+    // Buy credits button
+    $('.buyCreditsButton').on('click',function(){
+        var index = $('input[name="credits"]:checked').data('index');
+        $(buttons[index]).click();
+    });
+});
+</script>
--- wp-content/themes/storefront/page-591.php
+++ wp-content/themes/storefront/page-591.php
@@ -3,6 +3,16 @@ get_header();
 $current_user = wp_get_current_user();
 $url = get_home_url();
 ?>
+<div class="my-account__nav">
+<ul>
+		<li><a href="<?php echo $url . '/my-account'?>">Overview</a></li>
+		<li><a href="<?php echo $url . '/buy-credits'?>">Credits</a></li>
+		<li><a href="<?php echo $url . '/edit-account'?>">Profile</a></li>
+		<li><a class="active" href="<?php echo $url . '/orders'?>">Orders</a></li>
+		<li><a href="<?php echo $url . '/my-account'?>">Downloads</a></li>
+	</ul>
+	<span>Log out</span>
+</div>
 <?php
 /**
  * My Orders - Deprecated
@@ -32,63 +42,65 @@ $customer_orders = get_posts( apply_filters( 'woocommerce_my_account_my_orders_q
 
 if ( $customer_orders ) : ?>
 
-	<h2><?php echo apply_filters( 'woocommerce_my_account_my_orders_title', __( 'Recent orders', 'woocommerce' ) ); ?></h2>
+	<div class="orders table-responsive">
+		<h2><?php echo apply_filters( 'woocommerce_my_account_my_orders_title', __( 'Recent orders', 'woocommerce' ) ); ?></h2>
 
-	<table class="shop_table shop_table_responsive my_account_orders">
+		<table class="shop_table shop_table_responsive my_account_orders table">
 
-		<thead>
-			<tr>
-				<?php foreach ( $my_orders_columns as $column_id => $column_name ) : ?>
-					<th class="<?php echo esc_attr( $column_id ); ?>"><span class="nobr"><?php echo esc_html( $column_name ); ?></span></th>
-				<?php endforeach; ?>
-			</tr>
-		</thead>
-
-		<tbody>
-			<?php foreach ( $customer_orders as $customer_order ) :
-				$order      = wc_get_order( $customer_order );
-				$item_count = $order->get_item_count();
-				?>
-				<tr class="order">
+			<thead>
+				<tr>
 					<?php foreach ( $my_orders_columns as $column_id => $column_name ) : ?>
-						<td class="<?php echo esc_attr( $column_id ); ?>" data-title="<?php echo esc_attr( $column_name ); ?>">
-							<?php if ( has_action( 'woocommerce_my_account_my_orders_column_' . $column_id ) ) : ?>
-								<?php do_action( 'woocommerce_my_account_my_orders_column_' . $column_id, $order ); ?>
-
-							<?php elseif ( 'order-number' === $column_id ) : ?>
-								<a href="<?php echo esc_url( $order->get_view_order_url() ); ?>">
-									<?php echo _x( '#', 'hash before order number', 'woocommerce' ) . $order->get_order_number(); ?>
-								</a>
-
-							<?php elseif ( 'order-date' === $column_id ) : ?>
-								<time datetime="<?php echo esc_attr( $order->get_date_created()->date( 'c' ) ); ?>"><?php echo esc_html( wc_format_datetime( $order->get_date_created() ) ); ?></time>
-
-							<?php elseif ( 'order-status' === $column_id ) : ?>
-								<?php echo esc_html( wc_get_order_status_name( $order->get_status() ) ); ?>
-
-							<?php elseif ( 'order-total' === $column_id ) : ?>
-								<?php
-								/* translators: 1: formatted order total 2: total order items */
-								printf( _n( '%1$s for %2$s item', '%1$s for %2$s items', $item_count, 'woocommerce' ), $order->get_formatted_order_total(), $item_count );
-								?>
-
-							<?php elseif ( 'order-actions' === $column_id ) : ?>
-								<?php
-								$actions = wc_get_account_orders_actions( $order );
-	
-								if ( ! empty( $actions ) ) {
-									foreach ( $actions as $key => $action ) {
-										echo '<a href="' . esc_url( $action['url'] ) . '" class="button ' . sanitize_html_class( $key ) . '">' . esc_html( $action['name'] ) . '</a>';
-									}
-								}
-								?>
-							<?php endif; ?>
-						</td>
+						<th class="<?php echo esc_attr( $column_id ); ?>"><span class="nobr"><?php echo esc_html( $column_name ); ?></span></th>
 					<?php endforeach; ?>
 				</tr>
-			<?php endforeach; ?>
-		</tbody>
-	</table>
+			</thead>
+
+			<tbody>
+				<?php foreach ( $customer_orders as $customer_order ) :
+					$order      = wc_get_order( $customer_order );
+					$item_count = $order->get_item_count();
+					?>
+					<tr class="order">
+						<?php foreach ( $my_orders_columns as $column_id => $column_name ) : ?>
+							<td class="<?php echo esc_attr( $column_id );?>" data-title="<?php echo esc_attr( $column_name ); ?>">
+								<?php if ( has_action( 'woocommerce_my_account_my_orders_column_' . $column_id ) ) : ?>
+									<?php do_action( 'woocommerce_my_account_my_orders_column_' . $column_id, $order ); ?>
+
+								<?php elseif ( 'order-number' === $column_id ) : ?>
+									<a href="<?php echo esc_url( $order->get_view_order_url() ); ?>">
+										<?php echo _x( '#', 'hash before order number', 'woocommerce' ) . $order->get_order_number(); ?>
+									</a>
+
+								<?php elseif ( 'order-date' === $column_id ) : ?>
+									<time datetime="<?php echo esc_attr( $order->get_date_created()->date( 'c' ) ); ?>"><?php echo esc_html( wc_format_datetime( $order->get_date_created() ) ); ?></time>
+
+								<?php elseif ( 'order-status' === $column_id ) : ?>
+									<?php echo esc_html( wc_get_order_status_name( $order->get_status() ) ); ?>
+
+								<?php elseif ( 'order-total' === $column_id ) : ?>
+									<?php
+									/* translators: 1: formatted order total 2: total order items */
+									printf( _n( '%1$s for %2$s item', '%1$s for %2$s items', $item_count, 'woocommerce' ), $order->get_formatted_order_total(), $item_count );
+									?>
+
+								<?php elseif ( 'order-actions' === $column_id ) : ?>
+									<?php
+									$actions = wc_get_account_orders_actions( $order );
+		
+									if ( ! empty( $actions ) ) {
+										foreach ( $actions as $key => $action ) {
+											echo '<a href="' . esc_url( $action['url'] ) . '" class="button ' . sanitize_html_class( $key ) . '">' . esc_html( $action['name'] ) . '</a>';
+										}
+									}
+									?>
+								<?php endif; ?>
+							</td>
+						<?php endforeach; ?>
+					</tr>
+				<?php endforeach; ?>
+			</tbody>
+		</table>
+	</div>
 <?php endif; ?>
 
 
